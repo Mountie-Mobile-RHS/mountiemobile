6579bfc493793774890f5291b05f004e71e225e5
bubbarob19 <77255872+bubbarob19@users.noreply.github.com>
2023-09-24 00:07:20 -0500

    app routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mountiemobile/config/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {                         
+  final _appRouter = AppRouter();
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Testing',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("APP BAR"),
-        ),
-        body: Center(
-          child: Container(
-            child: const Text("Mountie Mobile"),
-          ),
-        ),
-      )
-    );
-  }
-}
+  App({super.key});            
+            
+  @override            
+  Widget build(BuildContext context){            
+    return MaterialApp.router(            
+      routerConfig: _appRouter.config(),         
+    );            
+  }            
+}    
 
